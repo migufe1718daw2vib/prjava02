f0c407f05610c8ab2bad9efce41da8a863b040d4
migufe1718daw2vib <49649888+migufe1718daw2vib@users.noreply.github.com>
2019-05-02 19:42:41 +0200

    Update Prjava02.java
    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjava02

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -25,6 +25,7 @@ public class Prjava02 {
 		System.out.println("Creació d'una branca del projecte prjava02");
         System.out.println("versió 0.1 del projecte prjava02"); 	
 >>>>>>> master
+System.out.println("Afegint codi a la branca01 del projecte prjava02");
         try {
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
